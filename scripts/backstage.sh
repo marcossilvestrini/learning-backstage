a9b03345eaf71f9cba82d0e093aafac64b5812ab
marcos.silvestrini <marcos.silvestrini@gmail.com>
2023-10-05 20:27:57 -0300

    Update files

--- scripts/backstage.sh
+++ scripts/backstage.sh
@@ -10,7 +10,9 @@ MULTILINE-COMMENT
 # Set language/locale and encoding
 export LANG=C
 
-cd /home/vagrant || exit
+# Set working directory
+WORKDIR=/home/vagrant
+cd "$WORKDIR" || exit
 
 # Install Node - https://github.com/nvm-sh/nvm#install--update-script
 
@@ -55,14 +57,17 @@ yarn install
 sudo chown -R  vagrant:vagrant /opt/backstage/
 
 # Set app-config.yaml
-cp /home/vagrant/configs/app-config.yaml  /opt/backstage/skynet
-SECRETS_FILE="security/backstage-secrets.yaml"
+cp "$WORKDIR/backstage/app-config.yaml"  /opt/backstage/skynet
+SECRETS_FILE="$WORKDIR/security/backstage-secrets.yaml"
 CONFIG_FILE="/opt/backstage/skynet/app-config.yaml"
 GITHUB_TOKEN=$(grep 'GITHUB_TOKEN' "$SECRETS_FILE" | awk '{print $2}')
 GITHUB_CLIENT_ID=$(grep 'GITHUB_CLIENT_ID' "$SECRETS_FILE" | awk '{print $2}')
 GITHUB_CLIENT_SECRET=$(grep 'GITHUB_CLIENT_SECRET' "$SECRETS_FILE" | awk '{print $2}')
 sed -i "s/\${AUTH_GITHUB_CLIENT_ID}/$GITHUB_CLIENT_ID/g" "$CONFIG_FILE"
 sed -i "s/\${AUTH_GITHUB_CLIENT_SECRET}/$GITHUB_CLIENT_SECRET/g" "$CONFIG_FILE"
+
+# Set packages/app/src/App.tsx
+cp "$WORKDIR/backstage/App.tsx" /opt/backstage/skynet/packages/app/src/App.tsx
 
 # Start app
 yarn dev
