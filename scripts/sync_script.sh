dd9816e13daed56667b0a4fa0b6567a5a940a5c5
marcos.silvestrini <marcos.silvestrini@gmail.com>
2023-10-19 23:46:48 -0300

    Update files

--- scripts/sync_script.sh
+++ scripts/sync_script.sh
@@ -2,8 +2,8 @@
 
 # Função para realizar a sincronização.
 sync_directories() {
-  rsync -av --exclude 'node_modules' --exclude 'dist-types' /home/vagrant/backstage/skynet/ /opt/backstage/skynet/
-  #rsync -av --exclude 'node_modules' --exclude 'dist-types' /opt/backstage/skynet/ /home/vagrant/backstage/skynet/
+  rsync -av --exclude 'node_modules' --exclude 'dist-types' --exclude='.git' /home/vagrant/backstage/skynet/ /opt/backstage/skynet/
+  #rsync -av --exclude 'node_modules' --exclude 'dist-types' --exclude='.git' /opt/backstage/skynet/ /home/vagrant/backstage/skynet/
 }
 
 # Chame a função de sincronização.
